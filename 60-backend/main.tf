bd58d016936cc4bbb8a1f3b312ccb569d74231b8
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-14 17:17:18 +0530

    docker

--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -15,3 +15,32 @@ module "backend" {
     }
   )
 }
+
+resource "null_resource" "backend" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = module.backend.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host     = module.backend.private_ip
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source      = "${var.backend_tags.Component}.sh"
+    destination = "/tmp/backend.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/backend.sh",
+      "sudo sh /tmp/backend.sh"
+    ]
+  }
+}
