2ecaaa351eef5929bb5e8295c322f265ad7ab28f
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-15 05:02:33 +0530

    terraform

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -4,4 +4,4 @@ component=$1
 environment=$2
 echo "Component: $component, Environment: $environment"
 dnf install ansible -y
-ansible pull -i localhost, -U https://github.com/kasinenisaikrishna/expense-ansible-roles-tf.git main.yaml
+ansible pull -i localhost, -U https://github.com/kasinenisaikrishna/expense-ansible-roles-tf.git main.yaml -e component=$component -e environment=$environment 
