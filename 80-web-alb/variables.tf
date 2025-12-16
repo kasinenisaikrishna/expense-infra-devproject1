f475a76d6404d9e4766b09ad7bd8f8a6291a086e
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-16 08:31:08 +0530

    terraform

---
+++ 80-web-alb/variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+variable "zone_name" {
+  default = "dawsconnect.org"
+}
+
+variable "web_alb_tags" {
+  default = {
+    Component = "web-alb"
+  }
+}
