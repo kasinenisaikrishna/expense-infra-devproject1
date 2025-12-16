5bd65896eab1d251afc0da2acf4033f4409a2149
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-16 07:53:19 +0530

    terraform

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -63,7 +63,16 @@ module "vpn_sg" {
   source       = "git::https://github.com/kasinenisaikrishna/terraform-aws-security-group-p1.git?ref=main"
   project_name = var.project_name
   environment  = var.environment
-  sg_name      = "vpn" #expense-dev-app-alb
+  sg_name      = "vpn" 
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+}
+
+module "web_alb_sg" {
+  source       = "git::https://github.com/kasinenisaikrishna/terraform-aws-security-group-p1.git?ref=main"
+  project_name = var.project_name
+  environment  = var.environment
+  sg_name      = "web_alb" 
   vpc_id       = local.vpc_id
   common_tags  = var.common_tags
 }
---
+++ 70-acm/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+variable "zone_name" {
+  default = "dawsconnect.org"
+}
+
+variable "zone_id" {
+  default = "Z033712422B61CO3J7OXS"
+}
